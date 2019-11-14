d828aeab09cfb3a1f87b05d7fc4b64ac12a7ab68
Diego Méndez Valverde <diegomvalverde@outlook.com>
2019-11-14 01:41:58 -0600

    Added get function for icap jsons

--- src/models/icap-model/icap.model.js
+++ src/models/icap-model/icap.model.js
@@ -6,7 +6,7 @@ let jwt = require('jsonwebtoken');
 
 module.exports = function (AppUser) {
 
-  // Create Reminder
+  // Create icap json
   AppUser.create_icap_form = (req, callback) =>
   {
     let {idUser, jsonForm, formType} = req.body
@@ -27,6 +27,33 @@ module.exports = function (AppUser) {
     http: { path: '/create', verb: 'post' },
     returns: { arg: 'message', type: 'string' },
   })
+
+    // Get icap json
+    AppUser.get_icap_form = (req, callback) =>
+    {
+      let {idUser, formType} = req.body
+      let ds = AppUser.dataSource;
+      let sql = `select get_icap_${formType}($1)`;
+  
+      ds.connector.execute(sql, [idUser], (err, data) => {
+  
+      if (err) { return }
+      else
+      {
+        callback(null, data[0][`get_icap_${formType}`])
+      }
+  
+    })
+  
+    // callback(null, 'test')
+    }
+  
+    AppUser.remoteMethod('get_icap_form', {
+      accepts: { arg: 'req', type: 'any', 'http': { source: 'req' } },
+      http: { path: '/get', verb: 'get' },
+      returns: { arg: 'response', type: 'string' },
+    })
+  
 
 
 };
